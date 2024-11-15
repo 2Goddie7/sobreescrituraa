1f8205adacc4c0ce150acdc0e42c8ba846c03920
Diego Mullo <diego.mullo@epn.edu.ec>
2024-11-15 12:31:25 -0500

    Sin Scanner

---
+++ src/herenciaclase/Defensa.java
@@ -0,0 +1,4 @@
+package herenciaclase;
+
+public class Defensa {
+}
---
+++ src/herenciaclase/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+
+    private String nombre, equipo;
+    private int edad;
+
+    public Main(String nombre, String equipo, int edad) {
+        this.nombre = nombre;
+        this.equipo = equipo;
+        this.edad = edad;
+    }
+
+    public void imprimir(){
+        
+    }
+
+    public static void main(String[] args) {
+
+    }
+}

cb104964dc7784c699caa6a46cefc5629d55780d
Diego Mullo <diego.mullo@epn.edu.ec>
2024-11-15 12:31:44 -0500

    Sin Scanner

--- src/herenciaclase/Jugador.java
+++ src/herenciaclase/Jugador.java
@@ -1,4 +1,62 @@
 package herenciaclase;
 
 public class Jugador {
+
+    protected String nombre, equipo;
+    protected int edad;
+
+    public Jugador(String nombre, String equipo, int edad) {
+        this.nombre = nombre;
+        this.equipo = equipo;
+        this.edad = edad;
+    }
+
+    public void imprimir(){
+        System.out.println("El nombre del equipo es: "+equipo);
+        System.out.println("El nombre del jugador es: "+nombre);
+        System.out.println("La edad del jugador es: "+edad);
+    }
+
+    public static class Portero extends Jugador{
+        private int atajadas;
+        private int golesREecibidos;
+        public Portero(String nombre, String equipo, int edad, int atajadas, int golesREecibidos) {
+            super(nombre, equipo, edad);
+            this.atajadas=atajadas;
+            this.golesREecibidos=golesREecibidos;
+        }
+
+        @Override
+        public void imprimir() {
+            super.imprimir();
+            System.out.println("Atajadas realizadas: "+atajadas);
+            System.out.println("Goles recibidos: "+golesREecibidos);
+        }
+    }
+
+    public static class Defensa extends Jugador{
+        private int bloqueos;
+        public Defensa (String nombre, String equipo, int edad, int bloqueos) {
+            super(nombre, equipo, edad);
+            this.bloqueos=bloqueos;
+        }
+
+        @Override
+        public void imprimir() {
+            super.imprimir();
+            System.out.println("El número de bloqueos del jugador son: "+bloqueos);
+        }
+    }
+
+    public static class Delantero extends Jugador{
+        public Delantero (String nombre, String equipo, int edad) {
+            super(nombre, equipo, edad);
+        }
+
+        @Override
+        public void imprimir() {
+            super.imprimir();
+        }
+    }
+
 }
--- src/herenciaclase/Main.java
+++ src/herenciaclase/Main.java
@@ -1,19 +1,24 @@
+package herenciaclase;
+import java.util.Scanner;
+
 public class Main {
 
-    private String nombre, equipo;
-    private int edad;
-
-    public Main(String nombre, String equipo, int edad) {
-        this.nombre = nombre;
-        this.equipo = equipo;
-        this.edad = edad;
-    }
-
-    public void imprimir(){
-        
-    }
-
     public static void main(String[] args) {
-
+        Scanner input = new Scanner(System.in);
+        //Super clase
+        Jugador player = new Jugador("Messi","Inter de Miami",38);
+        player.imprimir();
+        //Subclase portero con sus atributos
+        System.out.println();
+        Jugador.Portero player1 = new Jugador.Portero("Emiliano","Aston Villa",32,150,332);
+        player1.imprimir();
+        //Subclase defensa con sus atributos
+        System.out.println();
+        Jugador.Defensa player2 = new Jugador.Defensa("Cubarsí","FC Barcelona",17,242);
+        player2.imprimir();
+        //Subclase Delantero sin atributos extras atributos
+        System.out.println();
+        Jugador.Delantero player3= new Jugador.Delantero("Cristiano","Camello",39);
+        player3.imprimir();
     }
 }
